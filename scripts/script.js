a4913bb8e2294d1e7575a91502b2dd75a7497a6f
Gabriel Pacheco Lopes <74369233+gabrielopes7@users.noreply.github.com>
2021-11-10 19:21:08 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 let url1 =
   "https://servicodados.ibge.gov.br/api/v1/localidades/municipios?orderBy=nome";
+let data = new Date();
+
+let dataHoje = `${("00" + data.getDate()).slice(-2)}/${
+  data.getMonth() + 1
+}/${data.getFullYear()}`;
 
 function pegarCidade() {
   let nomeCidade = document.getElementById("nomeCidade").value;
@@ -14,21 +19,23 @@ function pegarCidade() {
         return;
       }
       let cidade = data.find((city) => city.nome == nomeCidade).id;
-      cidadePrevisao(cidade.toString());
+      cidadePrevisao(cidade);
     });
 }
 
 function cidadePrevisao(cidade) {
-  let url2 = "https://apiprevmet3.inmet.gov.br/previsao/" + cidade;
+  let url2 = "https://apiprevmet3.inmet.gov.br/previsao/" + cidade.toString();
   fetch(url2)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
+      let hojeManha = data[cidade][`${dataHoje}`].manha;
+      let hojeTarde = data[cidade][`${dataHoje}`].tarde;
+      let hojeNoite = data[cidade][`${dataHoje}`].noite;
+
+      console.log(data[cidade][`${dataHoje}`].manha.resumo); // Preciso agora transformar em uma função que mostrará no HTML
     });
 }
 
-function criarDiv(){
-
-}
+function criarDiv() {}
